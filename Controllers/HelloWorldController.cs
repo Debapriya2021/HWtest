ef2dd767cbcf75b531be39230e91f9724ca8ffef
Debapriya2021 <49976472+Debapriya2021@users.noreply.github.com>
2019-12-26 19:36:00 +0530

    temp

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HWTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        //private static readonly string hello = "hello-world";
+
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var obj = new Word();
+        //    obj.word = hello;
+        //    return new OkObjectResult(obj);
+        //}
+
+        [HttpGet]
+
+        public string Get()
+        {
+            var word = "Hello-World";
+            return word;
+        }
+
+    }
+}
---
+++ Word.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HWTask
+{
+    public class Word
+    {
+        public string word { get; set; }
+    }
+}
